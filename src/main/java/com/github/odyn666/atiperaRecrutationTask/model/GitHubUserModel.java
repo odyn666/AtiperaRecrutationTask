01b65d442ad232d7e2d4149c9d49d4dc9b71dedf
Kacper Hajduk <lambertmorhen@wp.pl>
2024-07-19 20:16:42 +0200

    crated GitHubUserModel class

---
+++ src/main/java/com/github/odyn666/atiperaRecrutationTask/model/GitHubUserModel.java
@@ -0,0 +1,14 @@
+package com.github.odyn666.atiperaRecrutationTask.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+
+public class GitHubUserModel {
+    public String login;
+
+
+
+}
