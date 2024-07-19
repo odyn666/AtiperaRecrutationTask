7df33d6f44e7d73a19704a9ef1842218b2dd77ee
Kacper Hajduk <lambertmorhen@wp.pl>
2024-07-19 20:17:26 +0200

    controller with header checking and usernameValidation added

---
+++ src/main/java/com/github/odyn666/atiperaRecrutationTask/controller/GitHubTaskController.java
@@ -0,0 +1,38 @@
+package com.github.odyn666.atiperaRecrutationTask.controller;
+
+import com.github.odyn666.atiperaRecrutationTask.dto.GitHubDTO;
+import com.github.odyn666.atiperaRecrutationTask.exception.BadHeaderException;
+import com.github.odyn666.atiperaRecrutationTask.exception.UserNotFoundException;
+import com.github.odyn666.atiperaRecrutationTask.service.GitHubService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class GitHubTaskController {
+    private final GitHubService gitHubService;
+
+    @GetMapping("/github/repositories")
+    public ResponseEntity<List<GitHubDTO>> getGitHubRepositories(
+            @RequestParam String username,
+            @RequestHeader("Accept") String acceptHeader
+    ) {
+
+        if (!acceptHeader.equals("application/json")) {
+            throw new BadHeaderException(HttpStatus.NOT_FOUND.value(), "INVALID ACCEPT HEADER");
+        }
+
+        gitHubService.validateUsername(username).orElseThrow(() -> new UserNotFoundException(HttpStatus.NOT_FOUND.value(), "USER NOT FOUND")); //validation that username is correct
+
+        List<GitHubDTO> repositories = gitHubService.getDTOs(username);
+
+        return ResponseEntity.ok(repositories);
+    }
+
+
+}
