390acfbf5099667a44b9ec23d6d4fe0673075f01
Kacper Hajduk <lambertmorhen@wp.pl>
2024-07-19 20:18:37 +0200

    initial commit

---
+++ src/test/java/com/github/odyn666/atiperaRecrutationTask/AtiperaRecrutationTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.odyn666.atiperaRecrutationTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaRecrutationTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
